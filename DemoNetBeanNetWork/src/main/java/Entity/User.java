e1e936158e712cd44d630afa75f95a3cbf57e4f7
Tran Bao Khanh <tbkhanh0908@gmail.com>
2022-07-24 20:39:06 +0700

    up

---
+++ DemoNetBeanNetWork/src/main/java/Entity/User.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entity;
+
+/**
+ *
+ * @author ASUS
+ */
+public class User {
+    private int userId;
+    private String userName;
+
+    public User(){}
+    public User(int userId) {
+        this.userId = userId;
+    }
+    public User(String userName) {
+        this.userName = userName;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
